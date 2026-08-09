7bd7e27cb2dcdddecf74ca04116d1d2e38435f20
onon1101 <mingzhuguly@gmail.com>
2026-08-09 17:08:17 +0800

    fix: 修正 logout 介面

--- auth/src/main/java/onon1101/lendingsystem/auth/logout/LogoutController.java
+++ auth/src/main/java/onon1101/lendingsystem/auth/logout/LogoutController.java
@@ -1,5 +1,8 @@
 package onon1101.lendingsystem.auth.logout;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import jakarta.validation.Valid;
 
 import onon1101.lendingsystem.configurations.controller.ApiResponse;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Tag(name = "auth", description = "認證相關 API")
 @RestController
 @RequestMapping("/api/v1/auth")
 public class LogoutController {
@@ -21,6 +26,11 @@ public class LogoutController {
         this.logoutService = logoutService;
     }
 
+    @Operation(
+            summary = "使用者登出",
+            description = "用於將 refresh-token 刪除，雖然使用者登出後，依然可以用 access token 登出。" +
+                    "但是仍舊過有效期限後，就會失效。"
+    )
     @PostMapping("/logout")
     public ResponseEntity<ApiResponse<Void>> logout(
             @Valid
